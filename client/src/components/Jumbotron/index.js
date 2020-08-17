9be822541dd0ea373530ad5cd30d1a5bdaf4a214
Nathan Szurek <nathan.szurek@yahoo.com>
2020-08-17 09:44:02 -0500

    Created basic pages and components

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Jumbotron() {
+    return (
+        <div>
+            <div class="jumbotron">
+                <h1 class="display-3">Google Book Search</h1>
+                <hr class="my-2"/>
+                <p>Find and save books to your reading list.</p>
+            </div>
+        </div>
+    )
+}
