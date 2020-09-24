3a9abf8f0d4f3061b665b4d7ab577f1f360b78cf
Nathan Szurek <nathan.szurek@yahoo.com>
2020-09-24 17:08:46 -0500

    Added notes to bookController for clarity

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,26 +2,31 @@ const db = require("../models");
 
 // Defining methods for the bookController
 module.exports = {
+  // Returns all books from the database
   findAll: function(req, res) {
     db.Book.find(req.query)
       .then(dbBook => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
+  // Returns one book from the database
   findById: function(req, res) {
     db.Book.findById(req.params.id)
       .then(dbBook => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
+  // Adds a book to the database
   create: function(req, res) {
     db.Book.create(req.body)
       .then(dbBook => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
+  // Updates an exisiting book in the database
   update: function(req, res) {
     db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
       .then(dbBook => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
+  // Deletes a book from the database
   remove: function(req, res) {
     db.Book.findById(req.params.id)
       .then(dbBook => dbBook.remove())
